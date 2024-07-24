06273d5fea87aacf9968e7ac9d3d3a49240b85dc
Rulopwd40 <132414402+Rulopwd40@users.noreply.github.com>
2024-07-24 17:16:05 -0300

    Login & Css

--- Gauchada.Frontend.Angular/src/app/Components/login/login.component.ts
+++ Gauchada.Frontend.Angular/src/app/Components/login/login.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl,ReactiveFormsModule } from '@angular/forms';
 
 @Component({
+  standalone:true,
   selector: 'app-login',
+  imports:[ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+    loginForm= new FormGroup({
+      username:new FormControl(''),
+      password:new FormControl('')
+      }
+    );
+  show= false;
+  login(){
+
+    }
+  register(){
+
+    }
+  showPassword(){
+    this.show=!this.show;
+  }
 }
